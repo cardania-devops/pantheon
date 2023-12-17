467709aeecb302da0ddbdbb08c6f4eda7c4d7dc0
Cardania DevOps <80608029+cardania-devops@users.noreply.github.com>
2023-12-17 03:19:34 -0800

    Update data_collector.py

--- src/data_collector.py
+++ src/data_collector.py
@@ -1,8 +1,12 @@
 # Collect and preprocess tweets
 from twitter_api import TwitterAPI
 import os
+import logging
 from .database_manager import DatabaseManager
-creds_path = os.getenv('GOOGLE_SHEETS_CREDENTIALS_PATH')
+
+# Setup basic logging
+logging.basicConfig(level=logging.INFO)
+
 class DataCollector:
     def __init__(self):
         api_key = os.getenv("TWITTER_API_KEY")
@@ -13,17 +17,27 @@ class DataCollector:
         self.twitter_api = TwitterAPI(api_key, api_secret_key, access_token, access_token_secret)
 
     def collect_tweets(self, cashtag, count=100):
-        # Fetch tweets with the cashtag
-        tweets = self.twitter_api.fetch_tweets_with_cashtag(cashtag, count)
-        # Here, you can add additional processing or filtering of tweets if necessary
-        return tweets
+        try:
+            # Fetch tweets with the cashtag
+            tweets = self.twitter_api.fetch_tweets_with_cashtag(cashtag, count)
+            # Additional processing or filtering of tweets can be added here
+            return tweets
+        except Exception as e:
+            logging.error(f"Error in collecting tweets: {e}")
+            return []
+
     def save_to_sheet(self, processed_data):
-        db_manager = DatabaseManager('path/to/your/credentials.json', 'Argus Sheet Name')
-        for data in processed_data:
-            db_manager.save_data(data)        
+        creds_path = os.getenv('GOOGLE_SHEETS_CREDENTIALS_PATH', 'path/to/your/credentials.json')
+        db_manager = DatabaseManager(creds_path, 'Argus Sheet Name')
+        try:
+            for data in processed_data:
+                db_manager.save_data(data)
+        except Exception as e:
+            logging.error(f"Error in saving data to sheet: {e}")
 
 # Example usage:
-collector = DataCollector()
-tweets = collector.collect_tweets("BTC", 10)
-for tweet in tweets:
-    print(tweet)
+if __name__ == "__main__":
+    collector = DataCollector()
+    tweets = collector.collect_tweets("BTC", 10)
+    for tweet in tweets:
+        print(tweet)
