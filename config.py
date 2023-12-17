f83adf4ce3341a3d28957d0513651e09f3840199
Cardania DevOps <80608029+cardania-devops@users.noreply.github.com>
2023-12-17 03:34:19 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,25 @@
 # Configuration variables and API keys
 import os
 
-# Configuration for Twitter API (Using Environment Variables)
-TWITTER_API_KEY = os.getenv('TWITTER_API_KEY')
-TWITTER_API_SECRET_KEY = os.getenv('TWITTER_API_SECRET_KEY')
-TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
-TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+# Fetch and validate Twitter API credentials
+TWITTER_API_KEY = os.getenv('TWITTER_API_KEY', 'default_twitter_api_key')
+TWITTER_API_SECRET_KEY = os.getenv('TWITTER_API_SECRET_KEY', 'default_twitter_api_secret_key')
+TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN', 'default_twitter_access_token')
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET', 'default_twitter_access_token_secret')
 
-# Configuration for Sentiment Analysis
-SENTIMENT_ANALYSIS_MODEL = 'gpt-4-1106-preview'  # Model used for sentiment analysis
+# Ensure that default values are replaced or removed in production
 
-# Other configuration parameters can be added here
+# Model used for sentiment analysis
+SENTIMENT_ANALYSIS_MODEL = os.getenv('SENTIMENT_ANALYSIS_MODEL', 'gpt-4-1106-preview')
+
+# Additional configuration parameters can be defined here
+
+# Example of separating development and production configurations
+if os.getenv('FLASK_ENV') == 'development':
+    # Development-specific configurations
+    DEBUG = True
+else:
+    # Production-specific configurations
+    DEBUG = False
+
+# Add more configurations as needed for the application
