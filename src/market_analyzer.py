cf24ab81301e33a36885d0f8bb30189d8919e88a
Cardania DevOps <80608029+cardania-devops@users.noreply.github.com>
2023-12-17 03:52:26 -0800

    Update market_analyzer.py

--- src/market_analyzer.py
+++ src/market_analyzer.py
@@ -1,29 +1,34 @@
-import requests
+import logging
 import os
+# Import necessary components from the Minswap SDK
+# from minswap_sdk import ...
 
 class MarketAnalyzer:
     def __init__(self):
-        self.base_url = os.getenv("MARKET_API_BASE_URL")
+        # Initialize any necessary configurations for Minswap API
+        self.minswap_client = ...  # Minswap client initialization
 
     def fetch_current_price(self, token):
         try:
-            response = requests.get(f"{self.base_url}/current_price/{token}")
-            response.raise_for_status()  # Raises a HTTPError if the HTTP request returned an unsuccessful status code
-            return response.json()
-        except requests.RequestException as e:
-            print(f"Error fetching current price: {e}")
+            # Use Minswap SDK to fetch current price
+            price = self.minswap_client.get_current_price(token)
+            return price
+        except Exception as e:
+            logging.error(f"Error fetching current price for {token}: {e}")
             return None
 
     def fetch_historical_data(self, token, start_date, end_date):
         try:
-            response = requests.get(f"{self.base_url}/historical_data/{token}?start={start_date}&end={end_date}")
-            response.raise_for_status()
-            return response.json()
-        except requests.RequestException as e:
-            print(f"Error fetching historical data: {e}")
+            # Use Minswap SDK to fetch historical data
+            historical_data = self.minswap_client.get_historical_data(token, start_date, end_date)
+            return historical_data
+        except Exception as e:
+            logging.error(f"Error fetching historical data for {token}: {e}")
             return None
 
+# Additional functions as needed based on Minswap SDK capabilities
+
 # Example usage
-analyzer = MarketAnalyzer()
-current_price = analyzer.fetch_current_price("BTC")
-historical_data = analyzer.fetch_historical_data("BTC", "2023-01-01", "2023-01-31")
+# analyzer = MarketAnalyzer()
+# current_price = analyzer.fetch_current_price("ADA")
+# historical_data = analyzer.fetch_historical_data("ADA", "2021-01-01", "2021-12-31")
