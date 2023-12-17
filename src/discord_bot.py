1a64119e2db98c530ee3fbf0606f25110ba7dc49
Cardania DevOps <80608029+cardania-devops@users.noreply.github.com>
2023-12-17 03:42:49 -0800

    Update discord_bot.py

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -1,10 +1,10 @@
 import discord
 import logging
 from discord.ext import commands
-from twitter_api import TwitterAPI  # Adjust this import based on your project structure
-from sentiment_analyzer import SentimentAnalyzer  # Adjust this import
-from market_analyzer import MarketAnalyzer  # Adjust this import
-from signal_generator import SignalGenerator  # Import the SignalGenerator class
+from twitter_api import TwitterAPI
+from sentiment_analyzer import SentimentAnalyzer
+from market_analyzer import MarketAnalyzer
+from signal_generator import SignalGenerator
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO, 
@@ -12,30 +12,12 @@ logging.basicConfig(level=logging.INFO,
                     filemode='a', 
                     format='%(asctime)s - %(levelname)s - %(name)s: %(message)s')
 
-async def on_command_error(self, ctx, error):
-    if isinstance(error, commands.CommandError):
-        logging.error(f"Command error in {ctx.command}: {error}")
-
-# Custom Help Command
 class CustomHelpCommand(commands.HelpCommand):
-    async def send_bot_help(self, mapping):
-        channel = self.get_destination()
-        for cog, commands in mapping.items():
-            filtered = await self.filter_commands(commands, sort=True)
-            command_signatures = [self.get_command_signature(c) for c in filtered]
-            if command_signatures:
-                cog_name = getattr(cog, "qualified_name", "No Category")
-                await channel.send(f"**{cog_name}**\n" + "\n".join(command_signatures))
-    # Implement other necessary methods like send_command_help, send_group_help, etc.
+    # ... existing CustomHelpCommand methods ...
 
-# Create the bot with specified intents
-intents = discord.Intents.default()
-intents.messages = True
-
-# Define the SignalBot class
 class SignalBot(commands.Bot):
     def __init__(self, command_prefix, token):
-        super().__init__(command_prefix=command_prefix, intents=intents, help_command=CustomHelpCommand())
+        super().__init__(command_prefix=command_prefix, intents=discord.Intents.default(), help_command=CustomHelpCommand())
         self.token = token
         self.twitter_api = TwitterAPI()  # Initialize the TwitterAPI
         self.sentiment_analyzer = SentimentAnalyzer()  # Initialize the SentimentAnalyzer
@@ -48,18 +30,24 @@ class SignalBot(commands.Bot):
     @commands.command(name='analyze', help='Analyzes the given cashtag')
     async def analyze(self, ctx, cashtag: str):
         try:
-            # Existing analyze command logic
-            # ...
+            # Logic to fetch data from Twitter based on cashtag
+            tweets = self.twitter_api.fetch_tweets_with_cashtag(cashtag)
+            # Analyze sentiment
+            sentiments = [self.sentiment_analyzer.analyze_sentiment(tweet) for tweet in tweets]
+            # Process and respond
+            # ... additional processing and response logic ...
+            response = "Analysis complete."
         except Exception as e:
             logging.error(f"Error in analyze command: {str(e)}")
             response = f"An error occurred: {str(e)}"
         await ctx.send(response)
 
     async def on_command_error(self, ctx, error):
-        # Existing global error handler
-        # ...
+        # Improved global error handler
+        if isinstance(error, commands.CommandError):
+            logging.error(f"Command error in {ctx.command}: {error}")
+            await ctx.send(f"An error occurred: {error}")
 
-# Main execution
 if __name__ == "__main__":
     bot = SignalBot(command_prefix="!", token="YOUR_DISCORD_BOT_TOKEN")
-    bot.run_bot()
+    bot.run(bot.token)
