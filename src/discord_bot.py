340fda655b6e9f7f491eed200e22330969136630
Cardania DevOps <80608029+cardania-devops@users.noreply.github.com>
2023-12-19 04:17:44 -0800

    Update discord_bot.py

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -1,11 +1,12 @@
 import discord
 import logging
-from discord.ext import commands
 from os import getenv
 from .twitter_api import TwitterAPI
 from .sentiment_analyzer import SentimentAnalyzer
 from .market_analyzer import MarketAnalyzer
 from .signal_generator import SignalGenerator
+from discord.ext import commands
+import json
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO,
@@ -18,17 +19,19 @@ class CustomHelpCommand(commands.HelpCommand):
 
 class SignalBot(commands.Bot):
     def __init__(self, command_prefix):
-        # ... other initialization code ...
+        super().__init__(command_prefix=command_prefix, intents=discord.Intents.default(), help_command=CustomHelpCommand())
+        
         twitter_api_key = getenv('TWITTER_API_KEY')
         twitter_api_secret_key = getenv('TWITTER_API_SECRET_KEY')
         twitter_access_token = getenv('TWITTER_ACCESS_TOKEN')
         twitter_access_token_secret = getenv('TWITTER_ACCESS_TOKEN_SECRET')
+
         self.twitter_api = TwitterAPI(api_key=twitter_api_key,
                                       api_secret_key=twitter_api_secret_key,
                                       access_token=twitter_access_token,
                                       access_token_secret=twitter_access_token_secret)
-        # ... rest of the initialization code ...
-
+        self.sentiment_analyzer = SentimentAnalyzer(getenv('OPENAI_API_KEY'))
+        # Initialize other components if necessary
 
     async def on_ready(self):
         logging.info(f"{self.user} has connected to Discord!")
@@ -38,11 +41,21 @@ class SignalBot(commands.Bot):
         try:
             tweets = self.twitter_api.fetch_tweets_with_cashtag(cashtag)
             sentiments = [self.sentiment_analyzer.analyze_sentiment(tweet) for tweet in tweets]
-            response = "Analysis complete."
+            response = "Analysis complete. Use the 'results' command to view the analysis."
         except Exception as e:
             logging.error(f"Error in analyze command: {str(e)}")
             response = f"An error occurred: {str(e)}"
         await ctx.send(response)
+
+    @commands.command(name='results', help='Displays the results of the last analysis')
+    async def display_results(self, ctx):
+        try:
+            with open('results.json', 'r') as file:
+                data = json.load(file)
+            message = f"Analysis for {data['cashtag']}:\n" + '\n'.join(data['sentiments'])
+            await ctx.send(message)
+        except Exception as e:
+            await ctx.send(f"Error retrieving results: {e}")
 
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandError):
@@ -55,4 +68,3 @@ if __name__ == "__main__":
         raise ValueError("DISCORD_BOT_TOKEN environment variable not set")
     bot = SignalBot(command_prefix="!")
     bot.run(token)
-
